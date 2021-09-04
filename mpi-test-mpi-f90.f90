467e47fa4a120ed22aa78d5fe3f3487470c27a29
Erik Schnetter <schnetter@gmail.com>
2021-09-03 21:17:22 -0400

    Correct syntax errors

--- mpi-test-mpi-f90.f90
+++ mpi-test-mpi-f90.f90
@@ -9,21 +9,21 @@ program mpi_test_mpi_f90
   integer status(MPI_STATUS_SIZE)
   integer ierror
 
-  printf '("MPI_Init")'
+  print '("MPI_Init")'
   call MPI_Init(ierror)
 
-  printf '("MPI_Comm_rank")'
+  print '("MPI_Comm_rank")'
   call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierror)
-  printf '("MPI_Comm_size")'
+  print '("MPI_Comm_size")'
   call MPI_Comm_size(MPI_COMM_WORLD, size, ierror)
   print '("size: ",i0,", rank: ",i0)', size, rank
 
   isend = 42
   irecv = -1
-  printf '("MPI_Send")'
+  print '("MPI_Send")'
   call MPI_Send(isend, 1, MPI_INTEGER, mod(rank + 1, size), 0, &
        MPI_COMM_WORLD, ierror)
-  printf '("MPI_Recv")'
+  print '("MPI_Recv")'
   call MPI_Recv(irecv, 1, MPI_INTEGER, mod(rank + size - 1, size), 0, &
        MPI_COMM_WORLD, status, ierror)
   if (isend /= 42) call MPI_Abort(MPI_COMM_WORLD, 1, ierror)
@@ -32,7 +32,7 @@ program mpi_test_mpi_f90
      call MPI_Abort(MPI_COMM_WORLD, 3, ierror)
   end if
   if (status(MPI_TAG) /= 0) call MPI_Abort(MPI_COMM_WORLD, 4, ierror)
-  printf '("MPI_Get_count")'
+  print '("MPI_Get_count")'
   call MPI_Get_count(status, MPI_INTEGER, count, ierror)
   if (count /= 1) call MPI_Abort(MPI_COMM_WORLD, 5, ierror)
   if (rank == 0) then
@@ -41,7 +41,7 @@ program mpi_test_mpi_f90
           status(MPI_SOURCE), status(MPI_TAG), status(MPI_ERROR), count
   end if
 
-  printf '("MPI_Finalize")'
+  print '("MPI_Finalize")'
   call MPI_Finalize(ierror)
 
 end program mpi_test_mpi_f90
